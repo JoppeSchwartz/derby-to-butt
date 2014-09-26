70a895f64d05bafe12eec98cf48f18c0f94393c2
Joseph Schwartz <joe@joeschwartz.com>
2014-09-26 10:31:10 -0400

    Basic version working.

--- Source/content_script.js
+++ Source/content_script.js
@@ -33,30 +33,30 @@ function handleText(textNode) {
 	var v = textNode.nodeValue;
 
   // Deal with the easy case
-  v = v.replace(/\b(T|t)he (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // t - 7 = m
-    // c - 1 = b
-    m = String.fromCharCode(p1.charCodeAt(0) - 7);
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
+  v = v.replace(/\b(R|r)oller (D|d)erby/g, function(match, p1, p2, offset, string) {
+    // r - 5 = m
+    // d - 2 = b
+    m = String.fromCharCode(p1.charCodeAt(0) - 5);
+    b = String.fromCharCode(p2.charCodeAt(0) - 2);
     return m + "y " + b + "utt";
   });
 
-  // Deal with private clouds
-  v = v.replace(/\b(P|p)rivate (C|c)loud/g, function(match, p1, p2, offset, string) {
-    // c - 1 = b
-    b = String.fromCharCode(p2.charCodeAt(0) - 1);
-    return b + "utt";
-  });
+  // // Deal with private derby
+  // v = v.replace(/\b(P|p)rivate (C|c)erby/g, function(match, p1, p2, offset, string) {
+  //   // c - 1 = b
+  //   b = String.fromCharCode(p2.charCodeAt(0) - 1);
+  //   return b + "utt";
+  // });
   // Get the corner cases
-  if(v.match(/cloud/i)) {
+  if(v.match(/derby/i)) {
     // If we're not talking about weather
-    if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
-      v = v.replace(/(C|c)loud/gi, function(match, p1, offset, string) {
+    //if(v.match(/PaaS|SaaS|IaaS|computing|data|storage|cluster|distributed|server|hosting|provider|grid|enterprise|provision|apps|hardware|software|/i)) {
+      v = v.replace(/(D|d)erby/gi, function(match, p1, offset, string) {
         // c - 1 = b
-        b = String.fromCharCode(p1.charCodeAt(0) - 1);
+        b = String.fromCharCode(p1.charCodeAt(0) - 2);
         return b + "utt";
       });
-    }
+    //}
   }
 	textNode.nodeValue = v;
 }
